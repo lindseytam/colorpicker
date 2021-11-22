cdce0d6ee0fc40c6758fec3aecb54b4cda6e67c7
Lindsey <lindseytam@gmail.com>
2021-11-22 15:25:17 -0800

    finalize hexagaon

--- src/lib/components/helpers/hexagon.js
+++ src/lib/components/helpers/hexagon.js
@@ -1,58 +1,65 @@
-import React from 'react';
-import styled from "styled-components";
+import React, { useState } from 'react';
+import styled, { ThemeProvider } from "styled-components";
 
-const Hexagon = ({height, width, shape, defaultColor, onChange}) => {
-    
-  const Hexagon = styled.input.attrs({ 
-    type: 'color',
-    // default: '#00aec9'
-  })`
-      height: 80px;
-      width: 60px;
-      background: #00aec9;
-      position: relative;
-      left: 25px;
-      box-sizing: border-box;
-    
-    &:before, &:after {
-      content: "";
-      position: absolute;
-      height: 0;
-      width:  0;
-      top:  0;
-      /* half height */
-      border-top: 40px solid transparent;
-      border-bottom: 40px solid transparent;
-    }
-    &:before {
-      left: -20px;
-      border-right: 20px solid #00aec9;
-    }
-    &:after {
-      right: -20px;
-      border-left: 20px solid #00aec9;
-    }         
-  `
+
+const HexagonInput = styled.input.attrs({ 
+  type: 'color',
+  placeholder: `${props => props.theme.color}`
+})`
+    height: ${props => props.theme.height}px;
+    width: ${props => props.theme.width}px;
+    background: ${props => props.theme.color};
+    position: relative;
+    left: 25px;
+    box-sizing: border-box;
   
-
-  const setDimensions = () => {
-    // if width is not specified, set width and height
-    if (!height && !width) {
-      height = "100px"
-      width = "100px"
-    } else if (!height) {
-        height = "100px"
-    } else if (!width) {
-        width = "100px"
-    }
+  &:before, &:after {
+    content: "";
+    position: absolute;
+    height: 0;
+    width:  0;
+    top:  0;
+    border-top: 40px solid transparent; //40
+    border-bottom: 40px solid transparent; //40
   }
+  &:before {
+    left: -20px;
+    border-right: 20px solid ${props => props.theme.color};
+  }
+  &:after {
+    right: -20px;
+    border-left: 20px solid ${props => props.theme.color};
+  }         
+`
 
-  setDimensions()
+HexagonInput.defaultProps = {
+  theme: {
+    color: "black",
+    height: 80,
+    width: 60
+  }
+}
 
+const Hexagon = ({height, width, defaultColor, onChange}) => {
+
+  const [color, setColor] = useState((defaultColor) ? "black" : defaultColor)
+
+  const theme = {
+    color: color,
+    height: height,
+    width: width
+  }
 
   return (
     <>
-      <Hexagon />
+    <ThemeProvider theme={theme}>
+      <HexagonInput 
+        onInput={(e) => {
+          setColor(e.target.value)
+          }
+        }
+      />
+      </ThemeProvider>
     </>
   );
 }
