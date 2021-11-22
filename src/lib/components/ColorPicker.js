8cc6190ffc96c59cb8ea36cc9b31dae96f86a74f
Lindsey <lindseytam@gmail.com>
2021-11-22 15:30:54 -0800

    clean up file, set default inputs

--- src/lib/components/ColorPicker.js
+++ src/lib/components/ColorPicker.js
@@ -2,37 +2,11 @@ import React from 'react';
 import './ColorPicker.css'
 import Hexagon from './helpers/hexagon'
 
-const ColorPicker = ({height, width, shape, defaultColor, onChange}) => {
+const ColorPicker = ({height=100, width=100, shape="quad", defaultColor="black", onChange}) => {  
 
-
-
-  const hexagon = () => ({
-    width: `200px`,
-    height: `200px`,
-    backgroundColor: `#1DA1F2`,
-    // backgroundImage: (isPreview) ? 'url(/path/to/image.jpg)' : 'none',
-  });
-  
-
-  const setDimensions = () => {
-    // if width is not specified, set width and height
-    if (!height && !width) {
-      height = "100px"
-      width = "100px"
-    } else if (!height) {
-        height = "100px"
-    } else if (!width) {
-        width = "100px"
-    }
-  }
-
-  setDimensions()
-
-  console.log("height-", height)
   return (
     <>
-      {/* <input className={shape} type="color" style={{'height': height, 'width': width}}/> */}
-      {shape === 'hexagon'? <Hexagon />:
+      {shape === 'hexagon'? <Hexagon width={width} height={height} defaultColor={"black"}/>:
       <input className={shape} type="color" style={{'height': height, 'width': width}}/>}
     </>
   );
