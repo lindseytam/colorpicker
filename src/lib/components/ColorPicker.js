6b6441f1bedfe631e419250d4d3c3fbd32fb676c
Lindsey <lindseytam@gmail.com>
2021-11-22 21:20:16 -0800

    import circle helper

--- src/lib/components/ColorPicker.js
+++ src/lib/components/ColorPicker.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './ColorPicker.css'
 import Hexagon from './helpers/hexagon'
+import Circle from './helpers/circle'
 
-const ColorPicker = ({height=100, width=100, shape="quad", defaultColor="black", onChange}) => {  
+const ColorPicker = ({height=100, width=100, diameter=100, shape="quad", defaultColor, onChange}) => {  
 
   return (
     <>
-      {shape === 'hexagon'? <Hexagon width={width} height={height} defaultColor={"black"}/>:
-      <input className={shape} type="color" style={{'height': height, 'width': width}}/>}
+      {shape === 'circle' && <Circle diameter={diameter} defaultColor={defaultColor}/>}
+      {shape === 'quad' && <input className={shape} type="color" style={{'height': height, 'width': width}}/>}
+      {shape === 'hexagon' && <Hexagon width={width} height={height} defaultColor={defaultColor}/>}
     </>
   );
 }
