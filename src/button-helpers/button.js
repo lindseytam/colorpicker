fcc05c69b70551d7986e15e782c4a10f9a2f02d4
Lindsey <lindseytam@gmail.com>
2021-12-05 11:45:47 -0800

    clean up button style

--- src/button-helpers/button.js
+++ src/button-helpers/button.js
@@ -6,11 +6,30 @@ import HexagonButton from './hexagon-button';
 
 const Buttons = ({shape, defaultColor, isActive=false}) => {
 
+  // renders a single button (active button)
+  const renderButton = () => {
+      return (
+        <>
+            {shape === 'square' && <SquareButton defaultColor={defaultColor} isActive={isActive}/>}
+            {shape === 'circle' && <CircleButton defaultColor={defaultColor} isActive={isActive}/>}
+            {shape === 'hexagon' && <HexagonButton defaultColor={defaultColor} isActive={isActive}/>}
+        </>
+      )
+  }
+  // renders an inactive button
+  const renderInactiveButton = () => {
+      return (
+        <div className='button ring' style={{backgroundColor: defaultColor}}>
+            <div className='button ring-inner' style={{backgroundColor: 'white'}}>
+                {renderButton()}
+            </div>
+        </div>
+      ) 
+  }
+  
   return (
-    <div className='button-container-internal' style={{backgroundColor: defaultColor}}>
-        {shape === 'square' && <SquareButton defaultColor={defaultColor} isActive={isActive}/>}
-        {shape === 'circle' && <CircleButton defaultColor={defaultColor} isActive={isActive}/>}
-        {shape === 'hexagon' && <HexagonButton defaultColor={defaultColor} isActive={isActive}/>}
+    <div className='button internal' style={{backgroundColor: isActive ? defaultColor : "white"}}>
+        {isActive ? renderButton(): renderInactiveButton()}
     </div>
   );
 }
